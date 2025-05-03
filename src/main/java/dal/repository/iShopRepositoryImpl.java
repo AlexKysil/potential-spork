6448df191079557f38d1e5921587036751db5460
AlexKysil <alex.kisel0504@gmail.com>
2025-05-03 13:03:11 +0200

    Fulfilled repository methods with logic

--- src/main/java/dal/repository/iShopRepositoryImpl.java
+++ src/main/java/dal/repository/iShopRepositoryImpl.java
@@ -2,41 +2,59 @@ package dal.repository;
 
 import dal.models.Customer;
 import dal.models.Product;
-import jakarta.websocket.server.ServerEndpoint;
+import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class iShopRepositoryImpl implements iShopRepository {
+
+    private EntityManager entityManager;
+
     @Override
     public Customer saveCustomer(Customer customer) {
-        return null;
+        entityManager.persist(customer);
+        return customer;
     }
 
     @Override
     public Optional<Customer> readCustomer(UUID uuid) {
-        return Optional.empty();
+        return Optional.of(entityManager.find(Customer.class, uuid));
     }
 
     @Override
     public void deleteCustomer(UUID uuid) {
+        val customer2Delete = readCustomer(uuid);
 
+        if(customer2Delete.isPresent()) {
+            entityManager.remove(customer2Delete.get());
+            System.out.println("Customer entity was deleted"); // TODO replace syout with logget once the logger added
+        }
     }
 
     @Override
     public Product saveProduct(Product product) {
-        return null;
+        entityManager.persist(product);
+        return product;
     }
 
     @Override
     public Optional<Product> readProduct(UUID uuid) {
-        return Optional.empty();
+        return Optional.of(entityManager.find(Product.class, uuid));
     }
 
     @Override
     public void deleteProduct(UUID uuid) {
+        val product2Delete = readProduct(uuid);
 
+        if(product2Delete.isPresent()) {
+            entityManager.remove(product2Delete.get());
+            System.out.println("Product entity was deleted"); // TODO replace syout with logget once the logger added
+        }
     }
 }
